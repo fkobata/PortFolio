6334ccbb513ec52bfa28f55f9a55dea8cf3965d5
fkobata <fernandolucas.kobata@gmail.com>
2024-04-16 21:16:22 -0300

    seção sobre e habilidades

--- static/script.js
+++ static/script.js
@@ -13,3 +13,16 @@ menuMobile.addEventListener("click", () => {
     body.classList.toggle("menu-nav-active")
 
 });
+
+
+/*Fecha menu quando clicar em algum item e muda o icone para list*/
+
+const navItem = document.querySelectorAll(".nav-item")
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x","bi-list");
+        }
+    })
+})
